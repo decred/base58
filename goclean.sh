05f1b8db51197a8db707e40331a1136fee7a0f97
Dave Collins <davec@conformal.com>
2020-06-07 22:54:29 -0500

    build: Update goclean.sh to match reality.
    This updates goclean.sh to match reality in terms of which linters it
    runs and makes it more consistent with dcrd while here by using a proper
    shebang as well as moving the set to the top.

--- goclean.sh
+++ goclean.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -ex
+
 # The script does automatic checking on a Go package and its sub-packages, including:
 # 1. gofmt         (http://golang.org/cmd/gofmt/)
 # 2. golint        (https://github.com/golang/lint)
 # 3. go vet        (http://golang.org/cmd/vet)
 # 4. gosimple      (https://github.com/dominikh/go-simple)
 # 5. unconvert     (https://github.com/mdempsky/unconvert)
-# 6. race detector (http://blog.golang.org/race-detector)
-# 7. test coverage (http://blog.golang.org/cover)
-
-set -ex
+# 6. ineffassign   (https://github.com/gordonklaus/ineffassign)
+# 7. race detector (http://blog.golang.org/race-detector)
 
 # run tests
 env GORACE="halt_on_error=1" go test -race ./...
