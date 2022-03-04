5b84bad1ebfd283724c012ec3e0e02f6f80b0ee2
Dave Collins <davec@conformal.com>
2022-03-04 10:51:29 -0600

    build: Update build tags to pref go1.17+ syntax.
    This updates the build tags used to generate the alphabet to the
    preferred syntax as of go 1.17+.

--- genalphabet.go
+++ genalphabet.go
@@ -1,9 +1,10 @@
 // Copyright (c) 2015 The btcsuite developers
-// Copyright (c) 2015 The Decred developers
+// Copyright (c) 2015-2022 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-//+build ignore
+//go:build ignore
+// +build ignore
 
 package main
 
