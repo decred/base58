c3252333e323d2001838f72d5a546319e5ecddb5
Dave Collins <davec@conformal.com>
2025-09-09 12:43:11 -0500

    build: Update golangci-lint to v2.4.0.
    This updates golangci-lint to v2.4.0 and adds an appropriate
    configuration file.
    
    It also adds several new linters and updates the script used to run
    tests and linters accordingly.

--- run_tests.sh
+++ run_tests.sh
@@ -2,30 +2,12 @@
 
 set -ex
 
-# The script does automatic checking on a Go package and its sub-packages, including:
-# 1. race detector (http://blog.golang.org/race-detector)
-# 2. gofmt         (http://golang.org/cmd/gofmt/)
-# 3. golint        (https://github.com/golang/lint)
-# 4. go vet        (http://golang.org/cmd/vet)
-# 5. gosimple      (https://github.com/dominikh/go-simple)
-# 6. unconvert     (https://github.com/mdempsky/unconvert)
-# 7. ineffassign   (https://github.com/gordonklaus/ineffassign)
-# 8. misspell      (https://github.com/client9/misspell)
-# 9. deadcode      (https://github.com/remyoudompheng/go-misc/tree/master/deadcode)
+# This script runs the tests for all packages in the repository and then uses
+# golangci-lint (github.com/golangci/golangci-lint) to run all linters defined
+# by the configuration in .golangci.yml.
 
 # run tests
 env GORACE="halt_on_error=1" go test -race ./...
 
-# golangci-lint (github.com/golangci/golangci-lint) is used to run each each
-# static checker.
-
-# check linters
-golangci-lint run --disable-all --deadline=10m \
-  --enable=gofmt \
-  --enable=revive \
-  --enable=vet \
-  --enable=gosimple \
-  --enable=unconvert \
-  --enable=ineffassign \
-  --enable=misspell \
-  --enable=deadcode
+# run linters
+golangci-lint run
